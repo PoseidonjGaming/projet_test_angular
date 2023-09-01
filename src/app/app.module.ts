79a64856b99ad67e417a5be4b2d996f0851c6211
Lucas Magnier <73874300+PoseidonjGaming@users.noreply.github.com>
2023-09-01 10:04:39 +0200

    add date to form series

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -13,22 +14,24 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { JwtModule } from '@auth0/angular-jwt';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EpisodeComponent } from './components/admin/episode/episode.component';
+import { AfficheDialogComponent } from './components/admin/series/affiche-dialog/affiche-dialog.component';
 import { SeriesComponent } from './components/admin/series/series.component';
 import { DetailSeriesComponent } from './components/detail-series/detail-series.component';
 import { HomeComponent } from './components/home/home.component';
 import { ExportDialogComponent } from './components/menu/export-dialog/export-dialog.component';
 import { ImportDialogComponent } from './components/menu/import-dialog/import-dialog.component';
 import { MenuComponent } from './components/menu/menu/menu.component';
-import { JwtModule } from '@auth0/angular-jwt';
 import { AuthInterceptor } from './interceptor/auth.interceptor';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { AfficheDialogComponent } from './components/admin/series/affiche-dialog/affiche-dialog.component';
+import { MatNativeDateModule } from '@angular/material/core';
 
 registerLocaleData(fr);
 
@@ -41,7 +44,8 @@ registerLocaleData(fr);
     DetailSeriesComponent,
     HomeComponent,
     SeriesComponent,
-    AfficheDialogComponent
+    AfficheDialogComponent,
+    EpisodeComponent
   ],
   imports: [
     BrowserModule,
@@ -62,6 +66,8 @@ registerLocaleData(fr);
     MatTableModule,
     MatListModule,
     MatSnackBarModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
