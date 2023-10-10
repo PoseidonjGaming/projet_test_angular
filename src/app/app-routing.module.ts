ad634a45854fabd2615230ab3c8e4bb3a8d6bf84
Lucas Magnier <73874300+PoseidonjGaming@users.noreply.github.com>
2023-10-10 16:42:25 +0200

    add character in detail series

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DetailSeriesComponent } from './components/detail-series/detail-series.component';
+import { DetailSeriesComponent } from './components/detail/detail-series/detail-series.component';
 import { HomeComponent } from './components/home/home.component';
 import { SeriesComponent } from './components/admin/series/series.component';
 import { SuperAdminGuard } from './guards/superAdmin/super-admin.guard';
@@ -10,6 +10,7 @@ import { CharacterComponent } from './components/admin/character/character.compo
 import { ProfileComponent } from './components/profile/profile.component';
 import { SearchComponent } from './components/search/search.component';
 import { CategoryComponent } from './components/admin/category/category.component';
+import { DetailActorComponent } from './components/detail/detail-actor/detail-actor.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -22,7 +23,12 @@ const routes: Routes = [
       { path: 'category', component: CategoryComponent }
     ]
   },
-  { path: 'detail/series/:id', component: DetailSeriesComponent },
+  {
+    path: 'detail', children: [
+      { path: 'series/:id', component: DetailSeriesComponent },
+      { path: 'actor/:id', component: DetailActorComponent }
+    ]
+  },
   { path: 'profile', component: ProfileComponent },
   { path: 'search', component: SearchComponent }
 ];
