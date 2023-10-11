5d4f5b82172e2e4d5b164f8443004d009a6473d4
Lucas Magnier <73874300+PoseidonjGaming@users.noreply.github.com>
2023-10-11 08:56:55 +0200

    up

--- src/app/components/detail/detail-actor/detail-actor.component.ts
+++ src/app/components/detail/detail-actor/detail-actor.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { mergeMap } from 'rxjs';
+import { Actor } from 'src/app/models/actor.model';
+import { ApiActorService } from 'src/app/service/actor/api-actor.service';
 
 @Component({
   selector: 'app-detail-actor',
   templateUrl: './detail-actor.component.html',
   styleUrls: ['./detail-actor.component.css']
 })
-export class DetailActorComponent {
+export class DetailActorComponent implements OnInit {
+  actor?: Actor
+
+
+  constructor(private service: ApiActorService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.pipe(
+      mergeMap((param: ParamMap)=>this.service.getById(param.get('id')!))
+    ).subscribe((dto: Actor) => {
+      console.log(dto);
+      
+    })
+  }
 
 }
