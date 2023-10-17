1ed65908ab37d2d746ac05cf611f4a5d62a1fc50
Lucas MAGNIER <lmagnier@dawan.fr>
2023-10-17 17:24:18 +0200

    wip admin movie

--- src/app/components/admin/movie/movie.component.ts
+++ src/app/components/admin/movie/movie.component.ts
@@ -1,7 +1,11 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, Inject, LOCALE_ID, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
+import { combineLatest, forkJoin } from 'rxjs';
+import { Category } from 'src/app/models/category.model';
 import { Movie } from 'src/app/models/movie.model';
 import { ApiService } from 'src/app/service/api.service';
+import { UtilsService } from 'src/app/service/utils/utils.service';
 
 @Component({
   selector: 'app-movie',
@@ -11,24 +15,82 @@ import { ApiService } from 'src/app/service/api.service';
 export class MovieComponent implements OnInit {
 
   movies: Movie[] = []
+  toAddMovie: Movie[] = []
+  categories: Category[] = []
   columns = ['name', 'releaseDate', 'action']
+  notification: number = 0
 
-  formMovie=new FormGroup({
-    
+  formMovie = new FormGroup({
+    id: new FormControl(null),
+    name: new FormControl(null),
+    summary: new FormControl(null),
+    releaseDate: new FormControl(new Date()),
+    categories: new FormControl(this.categories)
   })
 
-  constructor(private service: ApiService<Movie>, @Inject(LOCALE_ID) public locale: string) { }
+  constructor(private service: ApiService<Movie>,
+    private categoryService: ApiService<Category>,
+    private utilsService: UtilsService,
+    @Inject(LOCALE_ID) public locale: string
+  ) { }
+
+
   ngOnInit(): void {
-    this.service.getAll('movie').subscribe((dtos: Movie[]) => {
-      this.movies = dtos
+
+
+    combineLatest([
+      this.service.getAll('movie'),
+      this.categoryService.getAll('category')
+    ]).subscribe(([movieDtos, categoryDtos]) => {
+      this.movies = movieDtos
+      this.categories = categoryDtos
     })
   }
 
-  populate(movie: Movie) {
+  add() {
+
+  }
+
+  submit() {
+
+  }
+
+  saves() {
+
+  }
+
+  remove(index: number) {
 
   }
 
   deletes(movie: Movie) {
+
+  }
+
+  populate(movie: Movie) {
+    this.utilsService.populate(movie, this.formMovie)
+  }
+
+  updateNotif(event: number) {
+
+  }
+
+  drop(event: CdkDragDrop<Category[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+
+
+    }
+  }
+
+  search(term: string) {
 
   }
 }
