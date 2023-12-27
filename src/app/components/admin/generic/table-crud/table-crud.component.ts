fc1705f84766be5f8ff9e26c2bd415fbd705802d
Lucas Magnier <73874300+PoseidonjGaming@users.noreply.github.com>
2023-12-27 17:45:39 +0100

    fix table crud

--- src/app/components/admin/generic/table-crud/table-crud.component.ts
+++ src/app/components/admin/generic/table-crud/table-crud.component.ts
@@ -1,53 +1,112 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatTableModule } from '@angular/material/table';
-import { Observable, Subject, mergeMap } from 'rxjs';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { Base } from '../../../../models/base.model';
-import { PageResponse } from '../../../../models/pageResponse.model';
+import { CrudService } from '../../../../service/admin/crud/crud.service';
 import { ApiService } from '../../../../service/api.service';
+import { DataSource } from '@angular/cdk/collections';
+import { Observable, ReplaySubject } from 'rxjs';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { PageResponse } from '../../../../models/pageResponse.model';
+import { MatSortModule, Sort } from '@angular/material/sort';
+import { Sorter } from '../../../../models/Sorter.model';
+
 
 @Component({
   selector: 'app-table-crud',
   standalone: true,
-  imports: [MatTableModule, MatButtonModule],
+  imports: [MatTableModule, MatButtonModule, MatPaginatorModule, MatSortModule],
   templateUrl: './table-crud.component.html',
   styleUrl: './table-crud.component.css'
 })
 export class TableCRUDComponent implements OnInit {
 
-
   @Input({ required: true }) columns: { name: string, header: string }[] = []
   @Input({ required: true }) type = ''
 
-  @Output() populateEmitter = new EventEmitter<Base>()
+  @Output() populateEvent = new EventEmitter<Base>()
 
-  @Input({ required: true }) dataSource = new Subject<Base[]>()
 
-  constructor(private service: ApiService) { }
+  dataSource = new CustomDataSource()
+  paginator = { pageSize: 10, pageIndex: 0, length: 2 }
+
+  private sorting = { field: 'id', direction: Sorter.ASC }
+
+  constructor(private service: ApiService, private crudService: CrudService) { }
 
   ngOnInit(): void {
     this.columns.push({ name: 'action', header: 'Action' })
-    this.sendRequest().subscribe((dtos: PageResponse<Base>) => {
-      this.dataSource.next(dtos.content)
+    this.crudService.get().subscribe({
+      next: (value: Base) => {
+        this.sendRequest().subscribe((values: PageResponse<Base>) => {
+          this.dataSource.setData(values.content)
+          this.paginator.length = values.size
+        })
+      }
     })
+
+    this.crudService.next(new Base())
   }
 
-  populate(dto: Base) {
-    this.populateEmitter.emit(dto)
+  paginate(event: PageEvent) {
+    this.paginator.pageIndex = event.pageIndex
+    this.paginator.pageSize = event.pageSize
+
+    this.crudService.next(new Base())
   }
 
-  delete(dto: Base) {
-    this.service.delete(this.type, dto['id']).pipe(
-      mergeMap(() => this.sendRequest())
-    ).subscribe((dtos: PageResponse<Base>) => this.dataSource.next(dtos.content))
+  sort(event: Sort) {
+    this.sorting.field = (event.direction === '') ? 'id' : event.active
+    this.sorting.direction = (event.direction === '' || event.direction === 'asc') ? Sorter.ASC : Sorter.DESC
+
+    this.crudService.next(new Base())
   }
+
+  delete(base: Base) {
+    this.service.delete(this.type, base['id']).subscribe((value) => this.crudService.next(value))
+  }
+
+  populate(base: Base) {
+    this.populateEvent.emit(base)
+  }
+
+  private sendRequest() {
+    return this.service.sortPaged(this.type,
+      this.sorting.field, this.sorting.direction,
+      this.paginator.pageSize, this.paginator.pageIndex)
+  }
+
 
   getColumnNames() {
     return this.columns.map(c => c.name)
   }
-  sendRequest(): Observable<PageResponse<Base>> {
-    return this.service.getAllPaged(this.type, 10, 0)
+}
+
+class CustomDataSource extends DataSource<Base>{
+  private _dataStream = new ReplaySubject<Base[]>();
+  private datas: Base[] = []
+
+  constructor() {
+    super();
   }
+
+  connect(): Observable<Base[]> {
+    return this._dataStream;
+  }
+
+  disconnect() { }
+
+  setData(data: Base[]) {
+    this.datas = data
+    this._dataStream.next(data);
+  }
+
+  addData(value: Base) {
+    this.datas.push(value)
+    this.setData(this.datas)
+  }
+
+
 }
 
 
--- src/app/components/admin/series/series.component.ts
+++ src/app/components/admin/series/series.component.ts
@@ -1,86 +1,40 @@
 import { ScrollingModule } from '@angular/cdk/scrolling';
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
-import { Subject, endWith } from 'rxjs';
-import { Base } from '../../../models/base.model';
-import { Series } from '../../../models/series.model';
-import { UtilsService } from '../../../service/utils/utils.service';
 import { MenuComponent } from '../../menu/menu.component';
 import { DragAndDropComponent } from '../generic/drag-and-drop/drag-and-drop.component';
 import { FormComponent } from '../generic/form/form.component';
 import { TableCRUDComponent } from '../generic/table-crud/table-crud.component';
-import { ApiService } from '../../../service/api.service';
+import { ToAddTableComponent } from '../generic/to-add-table/to-add-table.component';
+import { MatButtonModule } from '@angular/material/button';
+import { CrudService } from '../../../service/admin/crud/crud.service';
+import { Series } from '../../../models/series.model';
+import { Base } from '../../../models/base.model';
 
 @Component({
   selector: 'app-series',
   standalone: true,
   imports: [MenuComponent,
     TableCRUDComponent,
+    ToAddTableComponent,
     FormComponent,
     DragAndDropComponent,
     ScrollingModule,
-    MatTabsModule
+    MatTabsModule,
+    MatButtonModule
   ],
   templateUrl: './series.component.html',
   styleUrl: './series.component.css'
 })
-export class SeriesComponent implements OnInit {
-  columns = [
+export class SeriesComponent {
+
+  columns: { name: string, header: string }[] = [
     { name: 'name', header: 'Nom' }
   ]
 
-  controls: { name: string, type: string }[] = []
+  constructor(private crudService: CrudService) { }
 
-  formSeries?: FormGroup
-
-  typeMap = new Map<string, string>()
-  displayMap = new Map<string, string>()
-
-  dataSource = new Subject<Base[]>()
-
-  constructor(private formBuilder: FormBuilder, private service: ApiService, private utilService: UtilsService) { }
-  ngOnInit(): void {
-    const dto = new Series()
-    this.formSeries = this.formBuilder.group(dto)
-    Object.keys(dto).forEach(property => {
-      if (property.endsWith('Ids')) {
-        this.formSeries?.get(property)?.setValue([])
-      }
-      this.controls.push({ name: property, type: typeof (dto[property]) })
-    })
-
-    this.formSeries.controls['name'].addValidators([Validators.required])
-
-    this.typeMap.set('nextMovieId', 'movie')
-    this.typeMap.set('nextSeriesId', 'series')
-    this.typeMap.set('previousMovieId', 'movie')
-    this.typeMap.set('previousSeriesId', 'series')
-    this.typeMap.set('categoryIds', 'category')
-
-    this.displayMap.set('nextMovieId', 'name')
-    this.displayMap.set('nextSeriesId', 'name')
-    this.displayMap.set('previousMovieId', 'name')
-    this.displayMap.set('previousSeriesId', 'name')
-
-  }
-
-  populate(dto: Base) {
-    if (this.formSeries) {
-      this.utilService.populate(dto, this.formSeries)
-      Object.keys(this.formSeries.controls).filter(control => control.endsWith('Ids')).forEach(controlName => {
-        const dragControl = this.formSeries?.get(controlName.slice(0, controlName.length - 3))
-        const controlIds = this.formSeries?.get(controlName)
-        const type = this.typeMap.get(controlName)
-        if (controlIds && dragControl && type) {
-          this.service.getByIds(type, controlIds.value).subscribe((dtos: Base[]) => {
-            dragControl.setValue(dtos)
-          })
-        }
-
-
-      })
-    }
-
+  populate(series: Base) {
+    console.log(series);
   }
 }
