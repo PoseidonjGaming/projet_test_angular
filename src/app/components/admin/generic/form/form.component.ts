faf93cc83f2d535662a34d06ce6380e569a35c31
Lucas Magnier <73874300+PoseidonjGaming@users.noreply.github.com>
2023-12-28 22:03:49 +0100

    fix reset select

--- src/app/components/admin/generic/form/form.component.ts
+++ src/app/components/admin/generic/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup, FormGroupDirective, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormGroupDirective, FormsModule, ReactiveFormsModule, Validator, ValidatorFn } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -10,6 +10,7 @@ import { Base } from '../../../../models/base.model';
 import { ApiService } from '../../../../service/api/api.service';
 import { UtilsService } from '../../../../service/api/utils/utils.service';
 import { SelectComponent } from './select/select.component';
+import { filter } from 'rxjs';
 
 
 @Component({
@@ -35,6 +36,7 @@ export class FormComponent implements OnInit {
   @Input({ required: true }) controls: { name: string, type: string }[] = []
   @Input({ required: true }) typeMap = new Map<string, string>()
   @Input({ required: true }) displayMap = new Map<string, string>()
+  @Input() validators: { controlName: string, validators: ValidatorFn[] }[] = []
 
   @Output() submitEvent = new EventEmitter<{ dto: Base, type: string }>()
 
@@ -46,9 +48,16 @@ export class FormComponent implements OnInit {
     if (this.form) {
       this.resetDto = this.form.value
       Object.keys(this.form.controls).filter(c => c.endsWith('Id')).forEach(controlIdName => {
-        const control = new FormControl([])
-        this.form?.addControl(controlIdName.slice(0, controlIdName.length - 2), control)
+          this.form?.addControl(controlIdName.slice(0, controlIdName.length - 2), new FormControl([]))
       })
+
+      this.validators.forEach(validator => {
+        let control = this.form?.get(validator.controlName)
+        if (control) {
+          control.addValidators(validator.validators)
+        }
+      })
+
     }
 
 
@@ -56,23 +65,30 @@ export class FormComponent implements OnInit {
   }
 
   submit(ngForm: FormGroupDirective) {
+    this.form?.markAllAsTouched()
     if (this.form && this.form.valid) {
-      this.submitEvent.emit({ dto: this.utilsService.updateValues(this.resetDto, this.form!), type: 'submit' })
-      ngForm.resetForm(this.resetDto)
+      this.submitEvent.emit({ dto: this.utilsService.updateValues(this.resetDto, this.form), type: 'submit' })
+      this.reset(ngForm)
     }
-
   }
 
   toAdd(ngForm: FormGroupDirective) {
     if (this.form) {
       this.submitEvent.emit({ dto: this.utilsService.updateValues(this.resetDto, this.form), type: 'toAdd' })
-      ngForm.resetForm(this.resetDto)
+      this.reset(ngForm)
       this.snack.open('Elément ajouté à la liste', 'Fermer', { duration: 5 * 1000 })
     }
   }
 
   reset(ngForm: FormGroupDirective) {
-    ngForm.resetForm(new Base())
+    Object.keys(this.resetDto).filter(e => e.endsWith('Id')).forEach(e => {
+      const controlName = e.slice(0, e.length - 2)
+      const control = this.form?.controls[controlName]
+      if (control) {
+        this.resetDto[controlName] = control.value
+      }
+    })
+    ngForm.resetForm(this.resetDto)
   }
 
   setPoster(event: Event) {
