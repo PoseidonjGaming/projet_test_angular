a363ea9127bf9c29468e2a475f91871dcebb769f
Lucas Magnier <73874300+PoseidonjGaming@users.noreply.github.com>
2023-12-28 19:31:30 +0100

    fix form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { DetailSeriesComponent } from './components/detail/detail-series/detail-series.component';
-import { SearchComponent } from './components/search/search.component';
-import { SeriesComponent } from './components/admin/series/series.component';
-import { EpisodeComponent } from './components/admin/episode/episode.component';
 import { ActorComponent } from './components/admin/actor/actor.component';
+import { CatagoryComponent } from './components/admin/catagory/catagory.component';
 import { CharacterComponent } from './components/admin/character/character.component';
+import { EpisodeComponent } from './components/admin/episode/episode.component';
 import { MovieComponent } from './components/admin/movie/movie.component';
+import { SeriesComponent } from './components/admin/series/series.component';
+import { DetailSeriesComponent } from './components/detail/detail-series/detail-series.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -18,7 +19,8 @@ export const routes: Routes = [
             { path: 'episodes', component: EpisodeComponent },
             { path: 'actors', component: ActorComponent },
             { path: 'characters', component: CharacterComponent },
-            { path: 'movies', component: MovieComponent }
+            { path: 'movies', component: MovieComponent },
+            { path: 'categories', component: CatagoryComponent }
         ]
     }
 ];
--- src/app/components/admin/catagory/catagory.component.ts
+++ src/app/components/admin/catagory/catagory.component.ts
@@ -1,12 +1,84 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../../menu/menu.component';
+import { FormComponent } from '../generic/form/form.component';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { TableCRUDComponent } from '../generic/table-crud/table-crud.component';
+import { ToAddTableComponent } from '../generic/to-add-table/to-add-table.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { Category } from '../../../models/category.model';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Base } from '../../../models/base.model';
+import { CrudService } from '../../../service/admin/crud/crud.service';
+import { ToAddService } from '../../../service/admin/toAdd/to-add.service';
+import { ApiService } from '../../../service/api/api.service';
+import { UtilsService } from '../../../service/api/utils/utils.service';
 
 @Component({
   selector: 'app-catagory',
   standalone: true,
-  imports: [],
+  imports: [
+    MenuComponent,
+    FormComponent,
+    TableCRUDComponent,
+    ToAddTableComponent,
+    ScrollingModule,
+    MatTabsModule
+  ],
   templateUrl: './catagory.component.html',
   styleUrl: './catagory.component.css'
 })
 export class CatagoryComponent {
+  type = 'category'
 
+  columns: { name: string, header: string }[] = [
+    { name: 'name', header: 'Nom' },
+    { name: 'action', header: 'Action' }
+  ]
+
+  controls: { name: string, type: string }[] = []
+  formCategory?: FormGroup
+  displayMap = new Map<string, string>()
+  typeMap = new Map<string, string>()
+
+
+
+  constructor(private formBuilder: FormBuilder,
+    private service: ApiService,
+    private crudService: CrudService,
+    private toAddService: ToAddService,
+    private utilsService: UtilsService) { }
+
+  ngOnInit(): void {
+
+    this.formCategory = this.formBuilder.group(new Category())
+
+    console.log(this.formCategory.value);
+
+    Object.keys(this.formCategory.controls).forEach(control => {
+      this.controls.push({ name: control, type: typeof (this.formCategory?.controls[control]) })
+    })
+  }
+
+  populate(series: Base) {
+    if (this.formCategory) {
+      this.utilsService.populate(series, this.formCategory)
+    }
+  }
+
+  submit(event: { dto: Base, type: string }) {
+    if (event.type === 'submit')
+      this.service.save(this.type, event.dto).subscribe((series) => {
+        this.crudService.next(series)
+      })
+    else
+      this.toAddService.next(event.dto)
+
+  }
+
+  saves(bases: Base[]) {
+    this.service.saves(this.type, bases).subscribe(() => {
+      this.crudService.next(new Category())
+      this.toAddService.next(new Category())
+    })
+  }
 }
