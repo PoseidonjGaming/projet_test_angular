e8f828e8e2ddea89c5277dd08de36c4b1337c9c2
Lucas Magnier <73874300+PoseidonjGaming@users.noreply.github.com>
2023-12-30 10:52:42 +0100

    add admin guard

---
+++ src/app/guard/admin/admin.guard.ts
@@ -0,0 +1,14 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { TokenService } from '../../service/api/token/token.service';
+
+export const adminGuard: CanActivateFn = (route, state) => {
+  const adminRoles=['ROLE_admin', 'ROLE_super_admin']
+  const tokenService = inject(TokenService)
+  return (tokenService.isExist()
+    && !inject(JwtHelperService).isTokenExpired(tokenService.getToken())
+    && adminRoles.includes(tokenService.getRole())) ? true :
+    inject(Router).createUrlTree(['/'])
+    ;;
+};
