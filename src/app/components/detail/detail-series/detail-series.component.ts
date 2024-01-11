726fedc4d1874ccb934cea75d233297f043dbcc3
Lucas Magnier <73874300+PoseidonjGaming@users.noreply.github.com>
2024-01-11 23:04:36 +0100

    wip review

--- src/app/components/detail/detail-series/detail-series.component.ts
+++ src/app/components/detail/detail-series/detail-series.component.ts
@@ -2,7 +2,7 @@ import { DatePipe } from '@angular/common';
 import { Component, Inject, Input, LOCALE_ID } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Actor } from '../../../models/actor.model';
 import { Character } from '../../../models/character.model';
 import { Episode } from '../../../models/episode.model';
@@ -11,7 +11,12 @@ import { Series } from '../../../models/series.model';
 import { ApiService } from '../../../service/api/api.service';
 import { MenuComponent } from '../../menu/menu.component';
 import { MatButtonModule } from '@angular/material/button';
-import { combineLatest, switchMap } from 'rxjs';
+import { combineLatest, mergeMap, switchMap } from 'rxjs';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { Review } from '../../../models/review.model';
+import { MatchMode } from '../../../models/enum/MatchMode.model';
+import { StringMatcher } from '../../../models/enum/StringMatcher.model';
 
 @Component({
   selector: 'app-detail-series',
@@ -19,6 +24,8 @@ import { combineLatest, switchMap } from 'rxjs';
   imports: [MatIconModule,
     MatExpansionModule,
     MatButtonModule,
+    MatTabsModule,
+    MatCardModule,
     RouterLink,
     MenuComponent,
     DatePipe],
@@ -27,28 +34,37 @@ import { combineLatest, switchMap } from 'rxjs';
 })
 export class DetailSeriesComponent {
 
-  @Input()
-  set id(seriesId: string) {
-    this.service.getById<Series>('series', Number.parseInt(seriesId)).subscribe((dto: Series) => {
-      this.series = dto
-    })
-  }
 
   series: Series = new Series()
   seasons: Season[] = []
   characters: Character[] = []
   actors: Actor[] = []
+  reviews: Review[] = []
 
 
-  constructor(private service: ApiService, @Inject(LOCALE_ID) public locale: string) { }
+  constructor(private service: ApiService, @Inject(LOCALE_ID) public locale: string, private route: ActivatedRoute) { }
   ngOnInit(): void {
-    combineLatest([
-      this.service.getByIds<Season>('season', this.series.seasonIds),
-      this.service.getByIds<Character>('character', this.series.characterIds)
-    ]).pipe(
-      switchMap(([seasonDtos, characterDtos]) => {
+    this.route.paramMap.pipe(
+      mergeMap(paramMap => {
+        return this.service.getById<Series>('series', Number.parseInt(paramMap.get('id')!))
+      }),
+      mergeMap(series => {
+        this.series = series
+        console.log(series.id);
+        
+        return combineLatest([
+          this.service.getByIds<Season>('season', this.series.seasonIds),
+          this.service.getByIds<Character>('character', this.series.characterIds),
+          this.service.search<Review>('review', { seriesId: series.id },
+            MatchMode.ANY, StringMatcher.EXACT, null, null)
+        ])
+      }),
+      switchMap(([seasonDtos, characterDtos, reviewDto]) => {
         this.characters = characterDtos
         this.seasons = seasonDtos
+        this.reviews = reviewDto
+        console.log(reviewDto);
+        
         return combineLatest([
           this.service.getByIds<Actor>('actor', characterDtos.map(e => e.actorId)),
           this.service.getByIds<Episode>('episode', seasonDtos.map(e => e.episodeIds).flat())
@@ -62,7 +78,6 @@ export class DetailSeriesComponent {
       this.characters.forEach(character => {
         character['actor'] = actorDtos.find(a => a.id == character.actorId)
       })
-
     })
 
   }
