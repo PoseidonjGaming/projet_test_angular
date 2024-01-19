fdc527c326053b992061a93c15f5ce6511abdba4
Lucas Magnier <73874300+PoseidonjGaming@users.noreply.github.com>
2024-01-19 09:34:25 +0100

    verify existing user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './service/api/api.service';
+import { TokenService } from './service/api/token/token.service';
+import { User } from './models/user.model';
+import { MatchMode } from './models/enum/MatchMode.model';
+import { StringMatcher } from './models/enum/StringMatcher.model';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +14,19 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'projet_test_angular';
+
+  constructor(private service: ApiService, private tokenService: TokenService) { }
+  ngOnInit(): void {
+    this.service.search<User>('user',
+      { username: this.tokenService.getUsername() },
+      MatchMode.ALL, StringMatcher.EXACT, null, null).subscribe(users => {
+        if (users.length == 0) {
+          this.tokenService.logout()
+        }
+        console.log(users);
+        
+      })
+  }
 }
